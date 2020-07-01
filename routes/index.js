8000f18599959197428e00aad541fea2803940fd
BrunoPanasio <brunowiz@gmail.com>
2020-07-01 16:38:52 -0300

    update heroku/github deploy auto

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'bruno panasio' });
+  res.render('index', { title: 'bruno panasio github keroku' });
 });
 
 module.exports = router;
